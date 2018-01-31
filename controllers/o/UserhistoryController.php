b1be1ee3d8180b5d6cf44a5795450d9280a4e8d0
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-02-01 06:13:00 +0700

    modify controller init

--- controllers/o/UserhistoryController.php
+++ controllers/o/UserhistoryController.php
@@ -43,8 +43,7 @@ class UserhistoryController extends Controller
 				$arrThemes = Utility::getCurrentTemplate('admin');
 				Yii::app()->theme = $arrThemes['folder'];
 				$this->layout = $arrThemes['layout'];
-			} else
-				throw new CHttpException(404, Yii::t('phrase', 'The requested page does not exist.'));
+			}
 		} else
 			$this->redirect(Yii::app()->createUrl('site/login'));
 	}
