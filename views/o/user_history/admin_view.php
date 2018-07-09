ba8904c1286a3e005d4383a840f7a7c0417a5047
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-09 15:52:47 +0700

    change dateFormat with traits

--- views/o/user_history/admin_view.php
+++ views/o/user_history/admin_view.php
@@ -37,7 +37,7 @@
 			),
 			array(
 				'name'=>'updated_date',
-				'value'=>!in_array($model->updated_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? Utility::dateFormat($model->updated_date, true) : '-',
+				'value'=>!in_array($model->updated_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00')) ? $this->dateFormat($model->updated_date, true) : '-',
 			),
 			array(
 				'name'=>'updated_id',
