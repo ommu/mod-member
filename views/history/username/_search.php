9275ae0b8804eb036b14940aff0fa47b45997538
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-10-31 06:01:09 +0700

    add profile in search history displayname & username

--- views/history/username/_search.php
+++ views/history/username/_search.php
@@ -17,6 +17,7 @@
 use Yii;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use ommu\member\models\MemberProfile;
 ?>
 
 <div class="member-history-username-search search-form">
@@ -28,6 +29,10 @@ use yii\widgets\ActiveForm;
 			'data-pjax' => 1
 		],
 	]); ?>
+
+		<?php $profile_id = MemberProfile::getProfile();
+		echo $form->field($model, 'profile_search')
+			->dropDownList($profile_id, ['prompt'=>'']);?>
 
 		<?php echo $form->field($model, 'member_search');?>
 
