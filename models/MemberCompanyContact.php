053c4ebea72eda2b651d1f0531d29159887070f3
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-11-01 19:49:27 +0700

    company contact models

---
+++ models/MemberCompanyContact.php
@@ -0,0 +1,307 @@
+<?php
+/**
+ * MemberCompanyContact
+ * 
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2018 Ommu Platform (www.ommu.co)
+ * @created date 1 November 2018, 19:36 WIB
+ * @link https://github.com/ommu/mod-member
+ *
+ * This is the model class for table "ommu_member_company_contact".
+ *
+ * The followings are the available columns in table "ommu_member_company_contact":
+ * @property integer $id
+ * @property integer $publish
+ * @property integer $status
+ * @property integer $member_company_id
+ * @property integer $contact_cat_id
+ * @property string $contact_value
+ * @property string $verified_date
+ * @property integer $verified_id
+ * @property string $creation_date
+ * @property integer $creation_id
+ * @property string $modified_date
+ * @property integer $modified_id
+ * @property string $updated_date
+ *
+ * The followings are the available model relations:
+ * @property MemberContactCategory $contact
+ * @property MemberCompany $memberCompany
+ * @property Users $creation
+ * @property Users $modified
+ *
+ */
+
+namespace ommu\member\models;
+
+use Yii;
+use yii\helpers\Url;
+use yii\helpers\Html;
+use ommu\users\models\Users;
+
+class MemberCompanyContact extends \app\components\ActiveRecord
+{
+	use \ommu\traits\UtilityTrait;
+
+	public $gridForbiddenColumn = [];
+
+	// Variable Search
+	public $verified_search;
+	public $creation_search;
+	public $modified_search;
+	public $member_search;
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public static function tableName()
+	{
+		return 'ommu_member_company_contact';
+	}
+
+	/**
+	 * @return \yii\db\Connection the database connection used by this AR class.
+	 */
+	public static function getDb()
+	{
+		return Yii::$app->get('ecc4');
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		return [
+			[['member_company_id', 'contact_cat_id', 'contact_value', 'verified_id'], 'required'],
+			[['publish', 'status', 'member_company_id', 'contact_cat_id', 'verified_id', 'creation_id', 'modified_id'], 'integer'],
+			[['contact_value'], 'string'],
+			[['verified_date', 'creation_date', 'modified_date', 'updated_date'], 'safe'],
+			[['contact_cat_id'], 'exist', 'skipOnError' => true, 'targetClass' => MemberContactCategory::className(), 'targetAttribute' => ['contact_cat_id' => 'cat_id']],
+			[['member_company_id'], 'exist', 'skipOnError' => true, 'targetClass' => MemberCompany::className(), 'targetAttribute' => ['member_company_id' => 'id']],
+		];
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id' => Yii::t('app', 'ID'),
+			'publish' => Yii::t('app', 'Publish'),
+			'status' => Yii::t('app', 'Status'),
+			'member_company_id' => Yii::t('app', 'Member Company'),
+			'contact_cat_id' => Yii::t('app', 'Contact Cat'),
+			'contact_value' => Yii::t('app', 'Contact Value'),
+			'verified_date' => Yii::t('app', 'Verified Date'),
+			'verified_id' => Yii::t('app', 'Verified'),
+			'creation_date' => Yii::t('app', 'Creation Date'),
+			'creation_id' => Yii::t('app', 'Creation'),
+			'modified_date' => Yii::t('app', 'Modified Date'),
+			'modified_id' => Yii::t('app', 'Modified'),
+			'updated_date' => Yii::t('app', 'Updated Date'),
+			'creation_search' => Yii::t('app', 'Creation'),
+			'verified_search' => Yii::t('app', 'Verified'),
+			'modified_search' => Yii::t('app', 'Modified'),
+			'member_search' => Yii::t('app', 'Member'),
+		];
+	}
+
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getContact()
+	{
+		return $this->hasOne(MemberContactCategory::className(), ['cat_id' => 'contact_cat_id']);
+	}
+
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getMemberCompany()
+	{
+		return $this->hasOne(MemberCompany::className(), ['id' => 'member_company_id']);
+	}
+
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getVerified()
+	{
+		return $this->hasOne(Users::className(), ['user_id' => 'verified_id']);
+	}
+
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getCreation()
+	{
+		return $this->hasOne(Users::className(), ['user_id' => 'creation_id']);
+	}
+
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getModified()
+	{
+		return $this->hasOne(Users::className(), ['user_id' => 'modified_id']);
+	}
+
+	/**
+	 * @inheritdoc
+	 * @return \ommu\member\models\query\MemberCompanyContact the active query used by this AR class.
+	 */
+	public static function find()
+	{
+		return new \ommu\member\models\query\MemberCompanyContact(get_called_class());
+	}
+
+	/**
+	 * Set default columns to display
+	 */
+	public function init() 
+	{
+		parent::init();
+
+		$this->templateColumns['_no'] = [
+			'header' => Yii::t('app', 'No'),
+			'class'  => 'yii\grid\SerialColumn',
+			'contentOptions' => ['class'=>'center'],
+		];
+		if(!Yii::$app->request->get('member')) {
+			$this->templateColumns['member_search'] = [
+				'attribute' => 'member_search',
+				'value' => function($model, $key, $index, $column) {
+					return isset($model->memberCompany) ? $model->memberCompany->member->displayname : '-';
+				},
+			];
+		}
+		if(!Yii::$app->request->get('contact')) {
+			$this->templateColumns['contact_cat_id'] = [
+				'attribute' => 'contact_cat_id',
+				'value' => function($model, $key, $index, $column) {
+					return isset($model->contact) ? $model->contact->title->message : '-';
+				},
+				'filter' => MemberContactCategory::getCategory(),
+			];
+		}
+		$this->templateColumns['contact_value'] = [
+			'attribute' => 'contact_value',
+			'value' => function($model, $key, $index, $column) {
+				return $model->contact_value;
+			},
+		];
+		$this->templateColumns['verified_date'] = [
+			'attribute' => 'verified_date',
+			'value' => function($model, $key, $index, $column) {
+				return !in_array($model->verified_date, ['0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00']) ? Yii::$app->formatter->format($model->verified_date, 'datetime') : '-';
+			},
+			'filter' => $this->filterDatepicker($this, 'verified_date'),
+			'format' => 'html',
+		];
+		if(!Yii::$app->request->get('verified')) {
+			$this->templateColumns['verified_search'] = [
+				'attribute' => 'verified_search',
+				'value' => function($model, $key, $index, $column) {
+					return isset($model->verified) ? $model->verified->displayname : '-';
+				},
+			];
+		}
+		$this->templateColumns['creation_date'] = [
+			'attribute' => 'creation_date',
+			'value' => function($model, $key, $index, $column) {
+				return !in_array($model->creation_date, ['0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00']) ? Yii::$app->formatter->format($model->creation_date, 'datetime') : '-';
+			},
+			'filter' => $this->filterDatepicker($this, 'creation_date'),
+			'format' => 'html',
+		];
+		if(!Yii::$app->request->get('creation')) {
+			$this->templateColumns['creation_search'] = [
+				'attribute' => 'creation_search',
+				'value' => function($model, $key, $index, $column) {
+					return isset($model->creation) ? $model->creation->displayname : '-';
+				},
+			];
+		}
+		$this->templateColumns['modified_date'] = [
+			'attribute' => 'modified_date',
+			'value' => function($model, $key, $index, $column) {
+				return !in_array($model->modified_date, ['0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00']) ? Yii::$app->formatter->format($model->modified_date, 'datetime') : '-';
+			},
+			'filter' => $this->filterDatepicker($this, 'modified_date'),
+			'format' => 'html',
+		];
+		if(!Yii::$app->request->get('modified')) {
+			$this->templateColumns['modified_search'] = [
+				'attribute' => 'modified_search',
+				'value' => function($model, $key, $index, $column) {
+					return isset($model->modified) ? $model->modified->displayname : '-';
+				},
+			];
+		}
+		$this->templateColumns['updated_date'] = [
+			'attribute' => 'updated_date',
+			'value' => function($model, $key, $index, $column) {
+				return !in_array($model->updated_date, ['0000-00-00 00:00:00','1970-01-01 00:00:00','0002-12-02 07:07:12','-0001-11-30 00:00:00']) ? Yii::$app->formatter->format($model->updated_date, 'datetime') : '-';
+			},
+			'filter' => $this->filterDatepicker($this, 'updated_date'),
+			'format' => 'html',
+		];
+		$this->templateColumns['status'] = [
+			'attribute' => 'status',
+			'filter' => $this->filterYesNo(),
+			'value' => function($model, $key, $index, $column) {
+				$url = Url::to(['status', 'id'=>$model->primaryKey]);
+				return $this->quickAction($url, $model->status, 'Verified,Unverified');
+			},
+			'contentOptions' => ['class'=>'center'],
+			'format' => 'raw',
+		];
+		if(!Yii::$app->request->get('trash')) {
+			$this->templateColumns['publish'] = [
+				'attribute' => 'publish',
+				'filter' => $this->filterYesNo(),
+				'value' => function($model, $key, $index, $column) {
+					$url = Url::to(['publish', 'id'=>$model->primaryKey]);
+					return $this->quickAction($url, $model->publish);
+				},
+				'contentOptions' => ['class'=>'center'],
+				'format' => 'raw',
+			];
+		}
+	}
+
+	/**
+	 * User get information
+	 */
+	public static function getInfo($id, $column=null)
+	{
+		if($column != null) {
+			$model = self::find()
+				->select([$column])
+				->where(['id' => $id])
+				->one();
+			return $model->$column;
+			
+		} else {
+			$model = self::findOne($id);
+			return $model;
+		}
+	}
+
+	/**
+	 * before validate attributes
+	 */
+	public function beforeValidate()
+	{
+		if(parent::beforeValidate()) {
+			if($this->isNewRecord)
+				$this->creation_id = !Yii::$app->user->isGuest ? Yii::$app->user->id : null;
+			else
+				$this->modified_id = !Yii::$app->user->isGuest ? Yii::$app->user->id : null;
+		}
+		return true;
+	}
+}
