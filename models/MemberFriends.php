9a22d0b7e3b512228db2308a8eb54147a1454b5c
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-12-07 05:33:16 +0700

    change db rules

--- models/MemberFriends.php
+++ models/MemberFriends.php
@@ -55,14 +55,6 @@ class MemberFriends extends \app\components\ActiveRecord
 	}
 
 	/**
-	 * @return \yii\db\Connection the database connection used by this AR class.
-	 */
-	public static function getDb()
-	{
-		return Yii::$app->get('ecc4');
-	}
-
-	/**
 	 * @return array validation rules for model attributes.
 	 */
 	public function rules()
